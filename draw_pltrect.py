bc1c757c4603109738b7bb765c3dee88ffc55ba3
fujitaken <kentyan199@yahoo.co.jp>
2019-03-28 11:58:51 +0900

    class functions

--- draw_pltrect.py
+++ draw_pltrect.py
@@ -2,158 +2,157 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-rect_list = []
+class DrawRectangle:
 
-# 押した時
-def Press(event):
-    global x1,y1,DragFlag,PlotFlag
+    def __init__(self,np_img):
+        # 初期値
+        self.x1  = 0
+        self.y1  = 0
+        self.x2  = 0
+        self.y2  = 0
 
-    # 丸める
-    cx = int(round(event.xdata))
-    cy = int(round(event.ydata))
+        # ドラッグしているかのフラグ
+        self.DragFlag = False
+        self.PlotFlag = False
 
-    x1 = cx
-    y1 = cy
+        # ソート
+        self.ix1, self.ix2 = sorted([self.x1,self.x2])
+        self.iy1, self.iy2 = sorted([self.y1,self.y2])
 
-    # フラグをたてる
-    DragFlag = True
-    PlotFlag = False
+        # plot
+        plt.close('all')
+        self.np_img = np_img
+        self.fig = None
+        self.ax2 = None
 
-# ドラッグした時
-def Drag(event):
-    global x1,y1,x2,y2,DragFlag
+        self.lns = []
+        self.rect_list = []
 
-    # ドラッグしていなければ終了
-    if DragFlag == False:
-        return
+    # 押した時
+    def Press(self,event):
+        # 丸める
+        cx = int(round(event.xdata))
+        cy = int(round(event.ydata))
 
-    # 丸める
-    cx = int(round(event.xdata))
-    cy = int(round(event.ydata))
+        self.x1 = cx
+        self.y1 = cy
 
-    x2 = cx
-    y2 = cy
+        # フラグをたてる
+        self.DragFlag = True
+        self.PlotFlag = False
 
-    # ソート
-    ix1, ix2 = sorted([x1,x2])
-    iy1, iy2 = sorted([y1,y2])
-
-    # 四角形を更新
-    DrawRect(x1,x2,y1,y2)
-
-    # 描画
-    plt.draw()
-
-# 離した時
-def Release(event):
-    global x1,y1,x2,y2,DragFlag,PlotFlag
-    # フラグをたおす
-    DragFlag = False
-    PlotFlag = True
-    rect_list.append([x1,y1,x2-x1,y2-y1])
-    print(rect_list)
-    #for l in rect_list:
-    DrawRect(x1,x2,y1,y2)
-    plt.draw()
-
-# キーボードのキーが押された時
-def onKey(event):
-    global lns,PlotFlag,fig
-    if event.key == 'a':
-        print("a is press!!")
-        rect_list.clear()
-        print(rect_list)
-        fig.delaxes(ax2)
-        lns.clear()
-        conf_plt(0,0,0,0)
+    # ドラッグした時
+    def Drag(self,event):
+        # ドラッグしていなければ終了
+        if self.DragFlag == False:
+            return
+        # 丸める
+        cx = int(round(event.xdata))
+        cy = int(round(event.ydata))
+        self.x2 = cx
+        self.y2 = cy
+        # ソート
+        self.ix1, self.ix2 = sorted([self.x1,self.x2])
+        self.iy1, self.iy2 = sorted([self.y1,self.y2])
+        # 四角形を更新
+        self.DrawRect(self.x1,self.x2,self.y1,self.y2)
+        # 描画
         plt.draw()
 
-# 四角形を描く関数
-def DrawRect(x1,x2,y1,y2):
-    global ax2,lns,PlotFlag
-    Rect = [ [ [x1,x2], [y1,y1] ],
-             [ [x2,x2], [y1,y2] ],
-             [ [x1,x2], [y2,y2] ],
-             [ [x1,x1], [y1,y2] ] ]
+    # 離した時
+    def Release(self,event):
+        # フラグをたおす
+        self.DragFlag = False
+        self.PlotFlag = True
+        self.rect_list.append([self.x1,self.y1,self.x2-self.x1,self.y2-self.y1])
+        print(self.rect_list)
+        #for l in rect_list:
+        self.DrawRect(self.x1,self.x2,self.y1,self.y2)
+        plt.draw()
 
-    for i, rect in enumerate(Rect):
-        if PlotFlag:
-            ax2.plot(rect[0],rect[1],color='r',lw=2)
-        else:
-            lns[i].set_data(rect[0],rect[1])
-    #print([x1,x2,y1,y2])
+    # キーボードのキーが押された時
+    def onKey(self,event):
+        # キーボードの'a'が押されたとき
+        if event.key == 'a':
+            print("a is press!!")
+            self.rect_list.clear()
+            print(self.rect_list)
+            self.fig.delaxes(self.ax2)
+            self.lns.clear()
+            self.conf_plt(0,0,0,0)
+            plt.draw()
+        # キーボードの'n'が押されたとき
+        if event.key == 'n':
+            print("n is press!!")
 
-# pltの初期化
-def conf_plt(x1,x2,y1,y2):
-    global DragFlag,fig,ax2
-    DragFlag = False
-    # plot
-    plt.close('all')
-    fig = plt.figure(figsize=(8,4))
-    
-    # subplot 1
-    #ax1 = fig.add_subplot(1,1,1)
-    ax2 = fig.add_subplot(1,1,1)
-    
-    # 画像を描画
-    #im1 = ax1.imshow(img)
-    
-    # 四角形を描画
-    Rect = [ [ [x1,x2], [y1,y1] ],
-             [ [x2,x2], [y1,y2] ],
-             [ [x1,x2], [y2,y2] ],
-             [ [x1,x1], [y1,y2] ] ]
-    
-    for rect in Rect:
-        ln, = ax2.plot(rect[0],rect[1],color='r',lw=2)
-        lns.append(ln)
-    #print(lns)
-    
-    # カラーマップの範囲を合わせる 
-    #ax1.set_clim(im1.get_clim())
-    
-    # 軸を消す
-    plt.axis('off')
-    
-    # 背景画像を表示
-    plt.imshow(img)
-    
-    # イベント
-    plt.connect('button_press_event', Press)
-    plt.connect('motion_notify_event', Drag)
-    plt.connect('button_release_event', Release)
-    plt.connect('key_press_event', onKey)
-    
-    plt.show()
+    # 四角形を描く関数
+    def DrawRect(self,x1,x2,y1,y2):
+        # 四角形の座標を指定
+        Rect = [ [ [x1,x2], [y1,y1] ],
+                 [ [x2,x2], [y1,y2] ],
+                 [ [x1,x2], [y2,y2] ],
+                 [ [x1,x1], [y1,y2] ] ]
+        for i, rect in enumerate(Rect):
+            if self.PlotFlag:
+                self.ax2.plot(rect[0],rect[1],color='r',lw=2)
+            else:
+                self.lns[i].set_data(rect[0],rect[1])
+                #print([x1,x2,y1,y2])
 
-# 画像を開く    
+    # pltの初期化
+    def conf_plt(self,x1,x2,y1,y2):
+
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+        self.DragFlag = False
+        # plot
+        plt.close('all')
+        self.fig = plt.figure(figsize=(8,4))
+
+        # subplot 1
+        #ax1 = fig.add_subplot(1,1,1)
+        self.ax2 = self.fig.add_subplot(1,1,1)
+
+        # 画像を描画
+        #im1 = ax1.imshow(img)
+
+        # 四角形を描画
+        Rect = [ [ [self.x1,self.x2], [self.y1,self.y1] ],
+                 [ [self.x2,self.x2], [self.y1,self.y2] ],
+                 [ [self.x1,self.x2], [self.y2,self.y2] ],
+                 [ [self.x1,self.x1], [self.y1,self.y2] ] ]
+
+        for rect in Rect:
+            ln, = self.ax2.plot(rect[0],rect[1],color='r',lw=2)
+            self.lns.append(ln)
+            #print(lns)
+
+        # カラーマップの範囲を合わせる
+        #ax1.set_clim(im1.get_clim())
+
+        # 軸を消す
+        plt.axis('off')
+
+        # 背景画像を表示
+        plt.imshow(self.np_img)
+
+        # イベント
+        plt.connect('button_press_event', self.Press)
+        plt.connect('motion_notify_event', self.Drag)
+        plt.connect('button_release_event', self.Release)
+        plt.connect('key_press_event', self.onKey)
+
+        plt.show()
+
+# 画像を開く
 from PIL import Image
 fnm = './gorillaface/gorillaface2.jpg'
 img = Image.open(fnm)
-
 # numpy.ndarrayに
 img = np.asarray(img)
 
-# 初期値
-x1  = 0 
-y1  = 0
-x2  = 0
-y2  = 0
-
-# ドラッグしているかのフラグ
-DragFlag = False
-
-# ソート
-ix1, ix2 = sorted([x1,x2])
-iy1, iy2 = sorted([y1,y2])
-
-# plot
-plt.close('all')
-fig = None
-# subplot 1
-#ax1 = fig.add_subplot(1,1,1)
-ax2 = None
-
-lns = []
-
-conf_plt(x1,x2,y1,y2)
+dr_rect = DrawRectangle(img)
+dr_rect.conf_plt(0,0,0,0)

07f5fe98cf604fb84d8b43558207c1cc78362a40
fujitaken <kentyan199@yahoo.co.jp>
2019-05-18 23:18:14 +0900

    change how to implementation by pandas

--- show_img_from_csv.py
+++ show_img_from_csv.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import csv
 import sys
@@ -23,10 +24,17 @@ def show_image(imgpath):
     plt.show()
 
 csv_file = sys.argv[1]  # input the file name which record the image name
-
+'''
 with open(csv_file) as f:
     reader = csv.reader(f)
     for row in reader:
         imgname = row[0]
         print(imgname)
         show_image(imgname)
+'''
+pd.set_option("display.max_colwidth", 120)  #pandasのカラム内の最大表示文字数
+image_paths = pd.read_csv(csv_file,header=None,encoding="shift-jis",names=('img',),nrows=20)
+for index,row in image_paths.iterrows():
+    imgname = row['img']
+    print(imgname)
+    show_image(imgname)

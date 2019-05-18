d745525316386269e7e5a7b84d3f2550d739b08f
fujitaken <kentyan199@yahoo.co.jp>
2019-05-18 22:51:00 +0900

    add key event

--- show_img_from_csv.py
+++ show_img_from_csv.py
@@ -4,10 +4,22 @@ import numpy as np
 import csv
 import sys
 
+del_flag = False
+
+def onKey(event):
+    if event.key == 'd':
+        print("this image delete csv!")
+        del_flag = True
+        plt.close('all')
+    if event.key == 'n':
+        print("go to next image")
+        plt.close('all')
+
 def show_image(imgpath):
     img = Image.open(imgpath)
     img_li = np.asarray(img)
     plt.imshow(img_li)
+    plt.connect('key_press_event',onKey)
     plt.show()
 
 csv_file = sys.argv[1]  # input the file name which record the image name
@@ -15,5 +27,6 @@ csv_file = sys.argv[1]  # input the file name which record the image name
 with open(csv_file) as f:
     reader = csv.reader(f)
     for row in reader:
-        imgname = row[0].replace(' ','')
+        imgname = row[0]
+        print(imgname)
         show_image(imgname)
